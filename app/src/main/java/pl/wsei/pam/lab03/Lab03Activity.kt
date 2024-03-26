8f6d9df82824c73e2dbda01fe24cdf9375d8ffba
Ptrk12 <random12@onet.pl>
2024-03-26 20:33:05 +0100

    lab3

--- app/src/main/java/pl/wsei/pam/lab03/Lab03Activity.kt
+++ app/src/main/java/pl/wsei/pam/lab03/Lab03Activity.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.widget.GridLayout
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import pl.wsei.pam.lab01.R
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 public class Lab03Activity : AppCompatActivity() {
     lateinit var mBoard: GridLayout
@@ -22,6 +25,33 @@ public class Lab03Activity : AppCompatActivity() {
         mBoard = findViewById(test)!!
         mBoard.columnCount = size[0]
         mBoard.rowCount = size[1]
+
         val mBoardModel = MemoryBoardView(mBoard, size[0], size[1])
+
+
+        runOnUiThread() {
+            mBoardModel.setOnGameChangeListener { e ->
+                run {
+                    when (e.state) {
+                        GameStates.Matching -> {
+                            e.tiles.map { it.revealed = true };
+                        }
+                        GameStates.Match -> {
+                            e.tiles.map { it.revealed = true };
+                        }
+                        GameStates.NoMatch -> {
+                            e.tiles.map { it.revealed = true };
+                            Timer().schedule(2000) {
+                                e.tiles.map { it.revealed = false };
+                            }
+                        }
+                        GameStates.Finished -> {
+                            Toast.makeText(this, "Game finished", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
